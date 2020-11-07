963e1d3f0c55cfb88026c6caa324fb384b407a28
James <james.mahy@cevo.co.uk>
2020-11-07 18:32:01 +0000

    New architecture fixes

--- src/services/General.js
+++ src/services/General.js
@@ -1,21 +1,20 @@
 import {Client} from "../Client.js";
+import {Message} from "../entities/Message";
+import {Request} from "../core/Request";
 
 export class GeneralService {
 
     /** @type {Client} */
     client = null;
-
+    
     constructor(client) {
         this.client = client;
+        this.provider = client.getProvider('content');
     }
+    
 
-
-    prepareUpload(callback, communityID) {
-        return client.emit(
-            'content/prepareUpload',
-            {community_id: communityID},
-            callback
-        );
+    prepareUpload(communityID) {
+        return new Request(this.provider, 'content', 'prepareUpload', {community_id: communityID}, 'POST', true).run().then((response) => response.data);
     }
 
 }
